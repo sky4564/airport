8ab5fc552f08d3bef3df7d6ae84af6e8e9696187
sky4564 <qw486512@gmail.com>
2025-07-03 15:58:39 +0900

    feat : add channel talk script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,54 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {/* 채널톡 위젯: pluginKey에 본인 키를 입력하세요 */}
+        <Script
+          id="channel-talk"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                var w = window;
+                if (w.ChannelIO) {
+                  return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+                }
+                var ch = function() {
+                  ch.c(arguments);
+                };
+                ch.q = [];
+                ch.c = function(args) {
+                  ch.q.push(args);
+                };
+                w.ChannelIO = ch;
+                function l() {
+                  if (w.ChannelIOInitialized) {
+                    return;
+                  }
+                  w.ChannelIOInitialized = true;
+                  var s = document.createElement('script');
+                  s.type = 'text/javascript';
+                  s.async = true;
+                  s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+                  var x = document.getElementsByTagName('script')[0];
+                  if (x.parentNode) {
+                    x.parentNode.insertBefore(s, x);
+                  }
+                }
+                if (document.readyState === 'complete') {
+                  l();
+                } else if (window.attachEvent) {
+                  window.attachEvent('onload', l);
+                } else {
+                  window.addEventListener('DOMContentLoaded', l, false);
+                  window.addEventListener('load', l, false);
+                }
+              })();
+              ChannelIO('boot', {
+                "pluginKey": "39320c76-d53e-4c6e-b412-921ceca3bcbb"
+              });
+            `,
+          }}
+        />
       </body>
     </html>
   );
